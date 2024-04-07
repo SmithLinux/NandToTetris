d53a122596ead12036c2328835fefdfacd7d9aa2
Lee <370528912@qq.com>
2024-04-07 23:52:55 +0800

    proj4: mult fix

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// input: the values store in the R0 and R1
+// the program is to multiplies R0 and R1 then stores the result in R2
+// Output R2
+
+// while R0 > 0:
+// 	R2 = R1 + R1
+//	R0--
+
+// set n = 0
+@n
+M=0
+
+(LOOP)
+// if n == R0 goto END
+@n
+D=M
+@R0
+D=D-M
+@END
+D;JEQ
+
+// R2 += R1
+
+// n = n + 1
+@n
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
